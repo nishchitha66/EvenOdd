b385ec90abc67205310b113dd99101ce95ec7850
nishchitha66 <nishchithas32@gmail.com>
2026-09-21 22:10:00 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,12 @@
+class Arrays{
+    public static void main(String[]args){
+        int a[]=new int[4];
+        a[0]=11;
+        a[1]=22;
+        a[2]=33;
+        a[3]=44;
+        for(int i=0;i<4;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
